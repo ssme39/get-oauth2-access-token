b66866d23b3e734616473943e82603e208838e6e
sergey <sergeysme39@gmail.com>
2021-03-18 23:51:55 +0200

    change redirect uri to live

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,6 @@
 export const config = {
     appId: '326b16bc-e55a-404a-b22d-9bdcd0b2aa61',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: 'https://get-oauth-access-token.herokuapp.com',
     scopes: [
         'Files.ReadWrite.All'
     ]
